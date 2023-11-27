442f67e58d75585996ccb864ddc758f6221220d1
almbur <albu2023@yahoo.com>
2023-11-26 20:47:35 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+
+
 module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
