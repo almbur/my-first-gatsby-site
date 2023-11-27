b3f0154b4d77ea01a4964236a2c59337220dbac4
almbur <albu2023@yahoo.com>
2023-11-26 21:23:51 -0500

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,15 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 
-const BlogPage = ({ data }) => {
+const BlogPage = () => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
+      <p>My cool posts will go in here</p>
     </Layout>
   )
 }
-
 export const query = graphql`
   query {
     allFile {
